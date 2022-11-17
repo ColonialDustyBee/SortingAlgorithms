db6b88f2b92c940221ce621ec161da603399410c
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-11-17 01:33:53 -0800

    Fixed QuickSort
    It should work properly now. The time is still the same.

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -1,11 +1,19 @@
+/*
+We each had a bit of trouble solving these on our own, so we used geeksforgeeks as a resource to help us out for each of these algorithms. The links are provided down below: 
+https://www.geeksforgeeks.org/bubble-sort/?ref=lbp - For Bubble Sort
+https://www.geeksforgeeks.org/insertion-sort/ - For Insertion Sort
+https://www.geeksforgeeks.org/quick-sort/?ref=lbp - For Quick Sort (this is for a pivot point being the last element)
+Also used this resource for QuickSort to understand the implementation of the frontmost element being the pivot point - https://www.youtube.com/watch?v=7h1s2SojIRw
+https://www.geeksforgeeks.org/selection-sort/?ref=lbp - For Selection Sort
+*/
+
 #pragma once
-// We should have different data sets of 100, 1000, and maybe 2500 respectively? We could always change the data size at any point.
 #include <vector>
 using namespace std;
 
 class SelectionSort {
 private:
-	int i = 0, j = 0; // meant to use for the for loops.
+	int i = 0, j = 0; 
 public:
 	SelectionSort(); 
 	void selectionSort(vector<int>);
@@ -18,7 +26,7 @@ public:
 	InsertionSort(); // default constructor
 	void insertionSort(vector<int>);
 };
-// Considered the slowest one here?
+// Considered the slowest one here
 // Compares two adjacent elements. If element adjacent to the other one is greater/less than the current one then it swaps the two, keeps going until it reaches the end.
 class BubbleSort { 
 private:
@@ -34,7 +42,7 @@ public:
 // increment i until you find element greater than pivot, decrement j until you find element smaller than the pivot.
 // should use two procedures, partition and the quicksort algorithm as well.
 // SHOULD STOP once j is greater than i and return the value of j.
-// Should start at the beginning for this algorithm
+// Should start at the beginning for this algorithm, there are versions where it starts at the end, the key feature 
 class QuickSort {
 private:
 	int i = 0, j = 0; // used for low and high points for pivot respectively.
