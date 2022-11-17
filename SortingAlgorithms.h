abc732b5ba7ecd4443d643fdfb7a506c2a17e841
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-11-16 23:39:22 -0800

    Updated Sorting Algorithms
    Quicksort has a few bugs and BubbleSort flat out doesn't work.

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -1,25 +1,46 @@
 #pragma once
-template <typename T>
+// We should have different data sets of 100, 1000, and maybe 2500 respectively? We could always change the data size at any point.
+#include <vector>
+using namespace std;
+
 class SelectionSort {
 private:
-	T minimumValue;
-	int i, j; // meant to use for the for loops.
-	vector<T> elements;
+	int i = 0, j = 0; // meant to use for the for loops.
 public:
-	SelectionSort();
-	~SelectionSort();
-	void selectionSort(T, elements);
-	void swap()
+	SelectionSort(); 
+	void selectionSort(vector<int>);
 };
-template <typename T> 
+// I think this is similar to sorting a deck of cards where you go one by one and place the card at the very end of the position and then then next position and what not.
 class InsertionSort {
-
+private:
+	int i = 0, j = 0; 
+public:
+	InsertionSort(); // default constructor
+	void insertionSort(vector<int>);
 };
-template <typename T>
+// Considered the slowest one here?
+// Compares two adjacent elements. If element adjacent to the other one is greater/less than the current one then it swaps the two, keeps going until it reaches the end.
+class BubbleSort { 
+private:
+	int i = 0, j = 0;
+public:
+	BubbleSort();
+	void bubbleSort(vector<int>);
+};
+// Recursive 
+// requires us to pick a pivot point and divide the vector into smaller pieces and then combine them all. 
+// requires low and high positions (beginning and end)
+// pivot point is all elements on left should be smaller and right should be bigger i < x < j. could be random as well.
+// increment i until you find element greater than pivot, decrement j until you find element smaller than the pivot.
+// should use two procedures, partition and the quicksort algorithm as well.
+// SHOULD STOP once j is greater than i and return the value of j.
+// Should start at the beginning for this algorithm
 class QuickSort {
-
-};
-template <typename T>
-class RadixSort {
-
+private:
+	int i = 0, j = 0; // used for low and high points for pivot respectively.
+public:
+	vector<int> elementsForQuickSort;
+	QuickSort(); // default constructor 
+	int partition(int, int); // used to split based on pivot point
+	void quickSort(int, int);
 };
